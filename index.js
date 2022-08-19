e4ec02dfd73fe6ec31abf835b86c11bc681ef07d
AliMukhtar99 <ali.mukhta@student.moringaschool.com>
2022-08-19 21:15:37 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+function superbowlWin(record) {
+    const result = record.find((currentRecord) => {
+      return currentRecord.result === "W";
+    });
+    if (result) {
+      return result.year;
+    }
+    return undefined;
+  }
